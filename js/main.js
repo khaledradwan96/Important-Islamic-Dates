018983b1fde4d600b9c8b4d50ed1ced6f4b52551
Khaled Radwan <khaledradwan96@gmail.com>
2024-07-11 15:39:34 +0300

    add section sliderDown for years (html + css + JS)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
+// ========== Navbar ==========
 $('#open-nav').click(function(){
-    $('#leftMenu').animate({width: `250`},1000)
+    $('#leftMenu').animate({width: `250`},500)
 })
 
-
 $('#leftMenu .fa-xmark').click(function(){
-    $('#leftMenu').animate({width: `0`},1000)
+    $('#leftMenu').animate({width: `0`},500)
+})
+
+// ========== Slider Down ==========
+$('#sliderDown h3').click(function(){
+  $(this).next().slideToggle(500)
+  $('.inner').not($(this).next()).slideUp(500)
 })
